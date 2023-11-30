8305ac3c82b51a46004d4966bc0f9a61cfa35f7f
21120171 <nguyendinhanhvlqt@gmail.com>
2023-12-01 02:57:44 +0700

    Adding feature: adding user album; TODO: adding photos to album and delete albums

--- app/src/main/java/edu/team08/infinitegallery/optionalbums/AlbumsFragment.java
+++ app/src/main/java/edu/team08/infinitegallery/optionalbums/AlbumsFragment.java
@@ -1,20 +1,26 @@
 package edu.team08.infinitegallery.optionalbums;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -63,6 +69,8 @@ public class AlbumsFragment extends Fragment {
             if (itemId == R.id.menuAlbumsSettings) {
                 Intent myIntent = new Intent(getActivity(), SettingsActivity.class);
                 startActivity(myIntent, null);
+            } else if (itemId == R.id.menuAlbumsAdd) {
+                addNewAlbum();
             }
             return true;
         });
@@ -73,6 +81,64 @@ public class AlbumsFragment extends Fragment {
         displayFolderAlbums();
 
         return rootView;
+    }
+
+    private void addNewAlbum() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Create New Album");
+
+        // Set up the input field
+        final EditText input = new EditText(context);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the positive and negative buttons
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String albumName = input.getText().toString().trim();
+
+                // Perform actions with the entered albumName
+                if (!TextUtils.isEmpty(albumName)) {
+                    // Add your logic here, e.g., create a new album with the entered name
+                    createNewAlbum(albumName);
+                } else {
+                    // Handle the case where the input is empty
+                    Toast.makeText(context, "Album name cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void createNewAlbum(String albumName) {
+        File albumsFolder = new File(Environment.getExternalStorageDirectory(), "Infinity-Albums");
+
+        // Create the Infinity-Albums folder if it doesn't exist
+        if (!albumsFolder.exists()) albumsFolder.mkdirs();
+
+        // Check if the albumName already exists in Infinity-Albums
+        File newAlbumFolder = new File(albumsFolder, albumName);
+        if (newAlbumFolder.exists()) {
+            Toast.makeText(context, "Album " + albumName + " already exists!", Toast.LENGTH_SHORT).show();
+        } else {
+            // Create the new album folder
+            if (newAlbumFolder.mkdirs()) {
+                Toast.makeText(context, "Album " + albumName + " created!", Toast.LENGTH_SHORT).show();
+                onResume();
+            } else {
+                Toast.makeText(context, "Failed to create album " + albumName, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -98,6 +164,22 @@ public class AlbumsFragment extends Fragment {
 
             // Add the photo to the corresponding folder
             folderMap.get(folder).add(photo);
+        }
+
+        // Read all user-album-folders and check if they exist in hashmap or not
+        File userAlbumsFolder = new File(Environment.getExternalStorageDirectory(), "Infinity-Albums");
+
+        if (userAlbumsFolder.exists() && userAlbumsFolder.isDirectory()) {
+            File[] userAlbumFolders = userAlbumsFolder.listFiles();
+
+            if (userAlbumFolders != null) {
+                for (File userAlbumFolder : userAlbumFolders) {
+                    // If the user album folder is not already in the map, add it with no photos
+                    if (!folderMap.containsKey(userAlbumFolder)) {
+                        folderMap.put(userAlbumFolder, new ArrayList<>());
+                    }
+                }
+            }
         }
 
         // Create AlbumFolder objects from the map entries
