b225ac6ffb626dc09d8ae6af08372685d589624a
21120171 <nguyendinhanhvlqt@gmail.com>
2023-12-04 01:08:44 +0700

    Add AddPhotoActivity

---
+++ app/src/main/java/edu/team08/infinitegallery/optionalbums/AddPhotoActivity.java
@@ -0,0 +1,16 @@
+package edu.team08.infinitegallery.optionalbums;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.team08.infinitegallery.R;
+
+public class AddPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_photo);
+    }
+}
